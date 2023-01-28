3ed8d698a82bfb84e50da7ef367398965d0241a7
fornewid <fornewid@users.noreply.github.com>
2023-01-28 18:11:25 +0000

    deploy: db28218e9396fef4bdb0bfaeb4fd65d97b2756bc

---
+++ api/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":photo-compose:dokkaHtmlPartial/androidTestRelease":[],":photo-compose:dokkaHtmlPartial/debug":[],":photo-compose:dokkaHtmlPartial/main":[],":photo-compose:dokkaHtmlPartial/release":[],":photo-compose:dokkaHtmlPartial/testFixtures":[],":photo-compose:dokkaHtmlPartial/testFixturesDebug":[],":photo-compose:dokkaHtmlPartial/testFixturesRelease":[]}'
